284d0d18c6954d19394f7b5110f3a6627f7d07c6
Drazcir Drakenflod <rickard.hallberg@hotmail.com>
2012-09-18 11:22:36 +0200

    added menu handling in mainActivity and added image in activity_main.xml

--- WorldOfEverbies/src/com/cheesymountain/woe/MainActivity.java
+++ WorldOfEverbies/src/com/cheesymountain/woe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.cheesymountain.woe;
 
+
+import android.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -10,11 +13,21 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //menu_feed
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+    
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch(item.getItemId()){
+    		case R.id.menu_feed:
+    			this.setContentView(R.layout.feed_menu);
+    			return true;
+    	}
+		return false;
     }
 }
