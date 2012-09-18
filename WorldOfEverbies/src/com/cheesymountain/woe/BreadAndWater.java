63ea625600b7da30719e7146fd5d7a56d8608ed7
Drazcir Drakenflod <rickard.hallberg@hotmail.com>
2012-09-18 15:44:13 +0200

    added constructor in BreadAndWater and made changes in activity_main.xml

--- WorldOfEverbies/src/com/cheesymountain/woe/BreadAndWater.java
+++ WorldOfEverbies/src/com/cheesymountain/woe/BreadAndWater.java
@@ -1,11 +1,17 @@
 package com.cheesymountain.woe;
 
 public class BreadAndWater extends Food {
-
+	
 	private String name = "Bread and Water";
 	private int cost = 0;
 	private int fullnessModifier = 20;
 	private int happinessModifier = -10;
+
+	public BreadAndWater(String name, int cost, int fullnessModifier,
+			int happinessModifier, int toxicityModifier) {
+		super(name, cost, fullnessModifier, happinessModifier, toxicityModifier);
+		// TODO Auto-generated constructor stub
+	}
 	
 	public String getName(){
 		return name;
