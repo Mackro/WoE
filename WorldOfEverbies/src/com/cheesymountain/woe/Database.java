402ab4869f101ee5e7ee8ee29f66688999a0e1b5
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-09-24 19:21:30 +0200

    first commit on new branch(hopefully). Started implementing save/load functionality, though not yet connected to MainActivity

---
+++ WorldOfEverbies/src/com/cheesymountain/woe/Database.java
@@ -0,0 +1,41 @@
+package com.cheesymountain.woe;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Context;
+
+public class Database extends Activity {
+	
+	private static final String MY_EVERBIE = "everbie_data";
+	
+	public void save() throws IOException, FileNotFoundException{
+		FileOutputStream fos = openFileOutput(MY_EVERBIE, Context.MODE_PRIVATE);
+		
+		Everbie everbie = Everbie.getEverbie();
+		String data = everbie.GetName()+
+				Integer.toString(everbie.getHealth())+
+				Integer.toString(everbie.getFullness())+
+				Integer.toString(everbie.getToxicity());
+		fos.write(data.getBytes());
+		fos.close();
+	}
+	
+	@SuppressWarnings("null")
+	public void load() throws IOException, FileNotFoundException{
+		FileInputStream fis = openFileInput(MY_EVERBIE);
+		byte[] newData = null;
+		fis.read(newData);
+		if(newData == null || newData.length == 0){
+			System.out.println("No Data Found when loading");
+		}
+		String dataString = "";
+		for (int i=0;i<newData.length;i++){
+			dataString = dataString+Byte.toString(newData[i]);
+		}
+		System.out.println(dataString);
+	}
+}
