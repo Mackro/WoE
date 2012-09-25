d9302211218696d63457108424b0f57412867a62
Emil ahsberg <lovet1991@hotmail.com>
2012-09-25 10:16:03 +0200

    Renamed variable in Everbie (needed by Thedor) and started on log.java class

--- WorldOfEverbies/src/com/cheesymountain/woe/Log.java
+++ WorldOfEverbies/src/com/cheesymountain/woe/Log.java
@@ -1,5 +1,18 @@
 package com.cheesymountain.woe;
 
+import java.util.LinkedList;
+
 public class Log {
+	
+	LinkedList<String> log;
+	
+	public Log(){
+		log = new LinkedList<String>();
+		genereateString();
+	}
+	
+	public String generateString(Food food){
+		return "";
+	}
 
 }
