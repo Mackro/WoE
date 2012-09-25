08ff0ef1412645a5ce99ed07686a38450a8ba88c
Mackro Otterberg <spamiuso@gmail.com>
2012-09-25 15:07:11 +0200

    removed commentaries in new everbie activity.

--- WorldOfEverbies/src/com/cheesymountain/woe/NewEverbieActivity.java
+++ WorldOfEverbies/src/com/cheesymountain/woe/NewEverbieActivity.java
@@ -13,10 +13,10 @@ public class NewEverbieActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		/*if(Everbie.exists()){
+		if(Everbie.exists()){
 			Intent main = new Intent("com.cheesymountain.woe.MAINACTIVITY");
 			startActivity(main);
-		}*/
+		}
 		setContentView(R.layout.activity_new_everbie);
 		((EditText)findViewById(R.id.everbieLongText)).setText("Mogno:\nThe Mogno race are known for their sharp calws and short temper.");
 	}
@@ -24,7 +24,7 @@ public class NewEverbieActivity extends Activity {
 	public void create(View view){
 		String name = ((EditText)findViewById(R.id.everbieNameText)).getText().toString();
 		Drawable image = ((ImageView)findViewById(R.id.everbiePicsImageView)).getDrawable();
-		Everbie.createEverbie(name/*, image */);
+		Everbie.createEverbie(name, image);
 		Intent main = new Intent("com.cheesymountain.woe.MAINACTIVITY");
 		startActivity(main);
 	}
