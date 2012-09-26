ce4b71f1edf9e65c5f95e7a3189b6bcf006e2ff1
Mackro Otterberg <spamiuso@gmail.com>
2012-09-26 14:06:22 +0200

    updated the method of setting image in new everbie

--- WorldOfEverbies/src/com/cheesymountain/woe/NewEverbieActivity.java
+++ WorldOfEverbies/src/com/cheesymountain/woe/NewEverbieActivity.java
@@ -10,6 +10,11 @@ import android.widget.ImageView;
 
 public class NewEverbieActivity extends Activity {
 
+private int[] imageId = {
+		R.drawable.wbe,
+};
+private int selectedImage = 0;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -19,12 +24,12 @@ public class NewEverbieActivity extends Activity {
 		}
 		setContentView(R.layout.activity_new_everbie);
 		((EditText)findViewById(R.id.everbieLongText)).setText("Mogno:\nThe Mogno race are known for their sharp calws and short temper.");
+		((ImageView)findViewById(R.id.everbiePicsImageView)).setImageResource(imageId[0]);
 	}
 	
 	public void create(View view){
 		String name = ((EditText)findViewById(R.id.everbieNameText)).getText().toString();
-		Drawable image = ((ImageView)findViewById(R.id.everbiePicsImageView)).getDrawable();
-		Everbie.createEverbie(name, image);
+		Everbie.createEverbie(name, getResources().getResourceEntryName(imageId[selectedImage]));
 		Intent main = new Intent("com.cheesymountain.woe.MAINACTIVITY");
 		startActivity(main);
 	}
