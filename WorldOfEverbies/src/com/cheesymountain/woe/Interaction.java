e8384ed0b0009acbf2be03a391ab446dc0dd8b35
Mackro Otterberg <spamiuso@gmail.com>
2012-10-01 16:48:27 +0200

    Started working on the feature Interaction

---
+++ WorldOfEverbies/src/com/cheesymountain/woe/Interaction.java
@@ -0,0 +1,53 @@
+package com.cheesymountain.woe;
+
+public abstract class Interaction {
+	private int happinessModifier;
+	private int charmModifier;
+	private int cutenessModifer;
+	
+	public Interaction(int happinessModifier){
+		this.happinessModifier = happinessModifier;
+		this.charmModifier = 0;
+		this.cutenessModifer = 0;
+	}
+	
+	/**
+	 * Sets the modifier for the charm attribute
+	 * @param charmModifier - the value to change with, default is 0
+	 */
+	protected void setCharmMod(int charmModifier){
+		this.charmModifier = charmModifier;
+	}
+	
+	/**
+	 * Sets the modifier for the cuteness attribute
+	 * @param cutenessModifier - the value to change with, default is 0
+	 */
+	protected void setCutenessMod(int cutenessModifier) {
+		this.cutenessModifer = cutenessModifier;
+	}
+	
+	/**
+	 * Returns the happiness modifier
+	 * @return the value to de-/increase happiness by
+	 */
+	public int getHappinessModifier(){
+		return this.happinessModifier;
+	}
+	
+	/**
+	 * Returns the charm modifier
+	 * @return the value to de-/increase charm by
+	 */
+	public int getCharmModifier(){
+		return this.charmModifier;
+	}
+	
+	/**
+	 * Returns the cuteness modifier
+	 * @return the value to de-/increase cuteness by
+	 */
+	public int getCutenessModifier(){
+		return this.cutenessModifer;
+	}
+}
---
+++ WorldOfEverbies/src/com/cheesymountain/woe/Snuggle.java
@@ -0,0 +1,9 @@
+package com.cheesymountain.woe;
+
+public class Snuggle extends Interaction {
+	
+	public Snuggle() {
+		super(2);
+	}
+
+}
