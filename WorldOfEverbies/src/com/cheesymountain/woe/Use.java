165db5a7f704689441f7899cb2470cd1c6dfd57b
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-10-02 15:12:45 +0200

    fixed the misrepresentation of stats and updated Use.java

--- WorldOfEverbies/src/com/cheesymountain/woe/Use.java
+++ WorldOfEverbies/src/com/cheesymountain/woe/Use.java
@@ -3,6 +3,7 @@ package com.cheesymountain.woe;
 import com.cheesymountain.woe.food.Food;
 import com.cheesymountain.woe.interact.Interaction;
 import com.cheesymountain.woe.work.Work;
+
 /**
  * @author Karl-Agnes
  *
@@ -40,11 +41,25 @@ public class Use {
 			Everbie.getEverbie().changeHappiness(interact.getHappinessModifier());
 		}
 		
-	/*	if(o instanceof Item){
+		if(o instanceof Item){
 			Item item = (Item)o;
 			Log.getLog().itemUsed(item);
 			Everbie.getEverbie().changeStrength(item.getStrengthModifier());
 			Everbie.getEverbie().changeIntelligence(item.getIntelligenceModifier());
-		}*/
+			Everbie.getEverbie().changeCharm(item.getCharmModifier());
+			Everbie.getEverbie().changeCuteness(item.getCutenessModifier());
+			Everbie.getEverbie().changeHappiness(item.getHeppinessModifier());
+			Everbie.getEverbie().changeHealth(item.getHealthModifier());
+			Everbie.getEverbie().changeStamina(item.getStaminaModifier());
+		}
+		
+		if(o instanceof Training){
+			Training train = (Training)o;
+			Log.getLog().trainingStarted(train);
+			Everbie.getEverbie().changeStrength(train.getStrengthModifier());
+			Everbie.getEverbie().changeStamina(train.getStaminaModifier());
+			Everbie.getEverbie().changeIntelligence(train.getIntelligenceModifier());
+			Everbie.getEverbie().changeFullness(train.getFullnessModifier());
+		}
 	}
 }
