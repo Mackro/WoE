88c2274453a1fe94daa03684d190fbfddf14dfff
Mackro Otterberg <spamiuso@gmail.com>
2012-10-02 12:09:17 +0200

    Refactored some variables into a new class names RaceDescription. Added license and javadoc to newEverbie

---
+++ WorldOfEverbies/src/com/cheesymountain/woe/RaceDescription.java
@@ -0,0 +1,17 @@
+package com.cheesymountain.woe;
+
+public class RaceDescription {
+
+	public static int[] IMAGE_ID = {
+		R.drawable.wbe,
+		R.drawable.ic_launcher,
+		R.drawable.ic_action_search,
+	};
+	public static String SWIPE_DESCRIPTION = "\n\n<swipe left/right to switch race>";
+	public static String[] IMAGE_DESCRIPTION = {
+		"Mogno:\nThe Mogno race are known for their sharp claws and short temper.",
+		"Rokash:\nThe Rokash race are known for beeing much cuter than the other races but also more hostile.",
+		"Skrom:\nThe Skrom race are known for their sharp intelect and oversized belly.",
+	};
+	
+}
