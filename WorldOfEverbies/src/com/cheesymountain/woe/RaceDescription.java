3ffe5c1b1b7462b1b95dd14b201d8a22d3ba3caf
Mackro Otterberg <spamiuso@gmail.com>
2012-10-05 10:40:27 +0200

    added new image, removed old image and decreased total application size from 4.7MB to 564kB

--- WorldOfEverbies/src/com/cheesymountain/woe/RaceDescription.java
+++ WorldOfEverbies/src/com/cheesymountain/woe/RaceDescription.java
@@ -11,7 +11,7 @@ public class RaceDescription {
 	 * The id of the different Everbie images
 	 */
 	public static int[] IMAGE_ID = {
-		R.drawable.wbe,
+		R.drawable.fee,
 		R.drawable.ic_launcher,
 		R.drawable.ic_action_search,
 	};
@@ -22,8 +22,9 @@ public class RaceDescription {
 	 * The descriptions to the different Everbie images
 	 */
 	public static String[] IMAGE_DESCRIPTION = {
-		"Mogno:\nThe Mogno race are known for their sharp claws and short temper.",
+		//"Mogno:\nThe Mogno race are known for their sharp claws and short temper.",
 		"Rokash:\nThe Rokash race are known for beeing much cuter than the other races but also more hostile.",
+		"Tragani:\nThe Tragani race are the fastest of all Everbies but lack intellegence to assess situations correcly.",
 		"Skrom:\nThe Skrom race are known for their sharp intelect and oversized belly.",
 	};
 	
