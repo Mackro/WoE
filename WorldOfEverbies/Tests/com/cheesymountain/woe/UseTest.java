7421e67ebfbc059fd74474ba28b7ac9fd9b1a37a
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-10-10 14:28:44 +0200

    initial commit to new branch

---
+++ WorldOfEverbies/Tests/com/cheesymountain/woe/UseTest.java
@@ -0,0 +1,38 @@
+package com.cheesymountain.woe
+
+import com.cheesymountain.woe.Everbie;
+
+public class UseTest {
+	
+	private Everbie everbie;
+	
+	public UseTest(){
+		everbie = Everbie.getEverbie();
+		testFood();
+		testWork();
+		testInteraction();
+		testItem();
+		testTraining();
+	}
+	
+	public void testFood(){
+		everbie.reset();
+		
+	}
+	
+	public void testWork(){
+		
+	}
+	
+	public void testInteraction(){
+		
+	}
+	
+	public void testItem(){
+		
+	}
+	
+	public void testTraining(){
+		
+	}
+}
