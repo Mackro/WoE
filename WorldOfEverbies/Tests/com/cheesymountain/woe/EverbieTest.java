b48a0743daca3ac919b373c8b9ffe9da6299b215
Drazcir Drakenflod <rickard.hallberg@hotmail.com>
2012-10-10 15:43:04 +0200

    started working on the everbie test

--- WorldOfEverbies/Tests/com/cheesymountain/woe/EverbieTest.java
+++ WorldOfEverbies/Tests/com/cheesymountain/woe/EverbieTest.java
@@ -1,5 +1,25 @@
 package com.cheesymountain.woe;
 
 public class EverbieTest {
+	
+	private Everbie everbie;
+	
+	public EverbieTest(){
+		
+		everbie = Everbie.getEverbie();
+		
+		testConstructor();
+		testVariables();
+	}
 
+	private void testConstructor(){
+		System.out.println("Creating Everbie = " + (everbie == null? "Failed, everbie = null": "Success"));
+	}
+	
+	private void testVariables() {
+
+		System.out.println("" + (everbie.getImageId() == 0?"Everbie has no image": "Everbie has image"));
+		System.out.println("" + (everbie.getName() == null?"Everbie has no name": "Everbie has name"));
+		
+	}
 }
