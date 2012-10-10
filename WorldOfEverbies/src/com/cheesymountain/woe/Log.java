b09beda38daa4b2755f631b28533c8ebc7ab6d26
Teodor Ostwald <teodor.ostwal@gmail.com>
2012-10-10 12:38:08 +0200

    added a timestamp in log

--- WorldOfEverbies/src/com/cheesymountain/woe/Log.java
+++ WorldOfEverbies/src/com/cheesymountain/woe/Log.java
@@ -1,6 +1,7 @@
 package com.cheesymountain.woe;
 
 import java.util.LinkedList;
+import android.text.format.*;
 
 import com.cheesymountain.woe.food.Food;
 import com.cheesymountain.woe.interact.Interaction;
@@ -19,10 +20,13 @@ public class Log {
 	private static Log log;
 	LinkedList<String> logList;
 	Everbie everbie;
+	Time time = new Time();
+	
 
 	private Log() {
 		logList = new LinkedList<String>();
 		everbie = Everbie.getEverbie();
+		time.set(System.currentTimeMillis());
 	}
 	
 	
@@ -60,9 +64,10 @@ public class Log {
 		if (logList.size() > 19) {
 			logList.removeFirst();
 		}
-		logList.addLast(everbie.getName() +" began to workout by starting with " + train.getName() + "\n");
+		logList.addLast(getTime() + everbie.getName() +" began to workout by starting with " + train.getName() + "\n");
 	}
-	
+
+
 	/**
 	 * Is called when work is started by the Everbie to configure the 
 	 * log correctly
@@ -72,7 +77,7 @@ public class Log {
 		if (logList.size() > 19) {
 			logList.removeFirst();
 		}
-		logList.addLast(everbie.getName() +" started to work as " + work.getName() + "\n");
+		logList.addLast(getTime() + everbie.getName() +" started to work as " + work.getName() + "\n");
 	}
 
 	/**
@@ -85,7 +90,7 @@ public class Log {
 		if (logList.size() > 19) {
 			logList.removeFirst();
 		}
-		logList.addLast(everbie.getName()+ " ate som " + food.getName() + " and is now"
+		logList.addLast(getTime() + everbie.getName()+ " ate som " + food.getName() + " and is now"
 				+ (food.getHappinessModifier()>0?" happier": (food.getHappinessModifier()<0?" angrier":" the same"))
 				+ (food.getToxicityModifier()>0?" but became sicker":(food.getToxicityModifier()<0?" and became healthier":"")) 
 				+ " than before and not as hungry \n");
@@ -101,7 +106,7 @@ public class Log {
 		if (logList.size() > 19) {
 			logList.removeFirst();
 		}
-		logList.addLast(everbie.getName() + " worked as " + work.getName() + " for " + work.getTime() 
+		logList.addLast(getTime() + everbie.getName() + " worked as " + work.getName() + " for " + work.getTime() 
 				+ " hours and has now become " + 
 				(work.getHappinessModifier()>0?" happier": (work.getHappinessModifier()<0?" angrier":" tired"))
 				+ " and earned " + work.getSalary());
@@ -118,7 +123,7 @@ public class Log {
 			logList.removeFirst();
 		}
 		
-		logList.addLast(everbie.getName() + " worked out by doing some " + train.getName() + " and now " + everbie.getName()
+		logList.addLast(getTime() + everbie.getName() + " worked out by doing some " + train.getName() + " and now " + everbie.getName()
 				+ " gained" + (train.getStrengthModifier()>0?" increased":train.getStrengthModifier()<0?" decreased":" no")
 				+ " strength and gained" + (train.getStaminaModifier()>0?" increased":train.getStaminaModifier()<0?" decreased":" no")
 				+ " stamina  and gained" + (train.getIntelligenceModifier()>0?" increased":train.getIntelligenceModifier()<0?" decreased":" no")
@@ -136,13 +141,12 @@ public class Log {
 			logList.removeFirst();
 		}
 
-		logList.addLast("You and " + everbie.getName() + " " + interact.getName() + " and now " + everbie.getName()
+		logList.addLast(getTime() + "You and " + everbie.getName() + " " + interact.getName() + " and now " + everbie.getName()
 				+ (interact.getCutenessModifier()>0?" became cuter":interact.getCutenessModifier()<0?" became uglier":" confused")
 				+ (interact.getCharmModifier()>0?" and whinks at you":interact.getCharmModifier()<0?" and gestures angrily at you"
 				:" and hugs you") + ", " + everbie.getName() + " is now"
 				+ (interact.getHappinessModifier()>0?" happier than":interact.getHappinessModifier()<0?" angrier than":" the same as") + " before");
 	}
-
 	
 	/**
 	 * Is called when item is given to the Everbie to configure the 
@@ -154,7 +158,7 @@ public class Log {
 		if (logList.size() > 19) {
 			logList.removeFirst();
 		}
-		logList.addLast(everbie.getName() + " used a " + item.getName() +  
+		logList.addLast(getTime() + everbie.getName() + " used a " + item.getName() +  
 		(item.getStrengthModifier()>0?" and became stronger":(item.getStrengthModifier()<0?" and became weaker":"")) + 
 		(item.getStaminaModifier()>0?" and became tougher":(item.getStaminaModifier()<0?" and became less fit":"")) + 
 		(item.getIntelligenceModifier()>0?" and became smarter":(item.getIntelligenceModifier()<0?" and became dumber":"")) +
@@ -162,5 +166,10 @@ public class Log {
 		(item.getCutenessModifier()>0?" and became cuter":(item.getCutenessModifier()<0?" and became uglier":"")) +
 		(item.getHealthModifier()>0?" and regained health":(item.getHealthModifier()<0?" and lost health":"")) +
 		(item.getToxicityModifier()>0?" and got sicker":(item.getStrengthModifier()<0?" and became healthier":"")) + "\n");
+	}
+	
+	private String getTime() {
+		String timeString = "\n"+time.monthDay+"/"+time.month+" "+time.hour+":"+time.minute+" - ";
+		return timeString;
 	}
 }
