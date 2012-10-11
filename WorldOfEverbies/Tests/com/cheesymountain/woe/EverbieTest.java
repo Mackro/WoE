011f6db30f7787b944f3fbd0fd41be8856467c7b
Drazcir Drakenflod <rickard.hallberg@hotmail.com>
2012-10-11 16:14:03 +0200

    extended the EverbieTest class

--- WorldOfEverbies/Tests/com/cheesymountain/woe/EverbieTest.java
+++ WorldOfEverbies/Tests/com/cheesymountain/woe/EverbieTest.java
@@ -1,41 +1,266 @@
 package com.cheesymountain.woe;
 
+import java.security.InvalidParameterException;
+
+import android.util.Log;
+
 public class EverbieTest {
 	
 	private Everbie everbie;
 	
 	public EverbieTest(){
 		
+		Log.i("Start", "Everbie Test started");
+		
 		everbie = Everbie.getEverbie();
 		
 		testConstructor();
-		testVariables();
-	}
-
-	private void testConstructor(){
-		System.out.println("Creating Everbie = " + (everbie == null? "Failed, everbie = null": "Success"));
+		testName();
+		testStartingVariables();
+		testChangeVariables();
+		testOccupiedSeconds();
+		testOccupiedMinutes();
+		testOccupiedHours();
+		testResetEverbie();
+		testRestoreEverbie();
+		
+		Log.i("Start", "Everbie Test complete");
 	}
 	
-	private void testVariables() {
+	/**
+	 * Method for testing the constructor in Everbie
+	 */
+	private void testConstructor(){
+		Log.i("Start", "Test constructor started");
+		
+		if(everbie == null){
+			Log.e("Error", "Everbie is null");
+		}
+		
+		Log.i("Complete", "Test constructor complete");
+	}
+	
+	/**
+	 * Method for testing the starting variables in Everbie
+	 */
+	private void testStartingVariables() {
+		
+		Log.i("Start", "Test starting variabes started");
 
-		System.out.println("" + (everbie.getImageId() == 0?"Everbie has no image": "Everbie has image"));
-		System.out.println("" + (everbie.getCharm() == 0?"Everbie has no Charm": "Everbie has charm"));
-		System.out.println("" + (everbie.getCuteness() == 0?"Everbie has no Cuteness": "Everbie has Cuteness"));
-		System.out.println("" + (everbie.getFullness() == 0?"Everbie has no Fullness": "Everbie has fullness"));
-		System.out.println("" + (everbie.getHappiness() == 0?"Everbie has no Happiness": "Everbie has Happiness"));
-		System.out.println("" + (everbie.getHealth() == 0?"Everbie is dead": "Everbie has Life"));
-		System.out.println("" + (everbie.getIntelligence() == 0?"Everbie has no Intelligence": "Everbie has Smart"));
-		System.out.println("" + (everbie.getLevel() == 0?"Everbie has no Level": "Everbie has Level"));
-		System.out.println("" + (everbie.getMaxHealth() == 0?"Everbie has no MaxHealth": "Everbie has MaxHealth"));
-		System.out.println("" + (everbie.getMoney() == 0?"Everbie has no Money": "Everbie has Money"));
-		System.out.println("" + (everbie.getStamina() == 0?"Everbie has no Stamina": "Everbie has Stamina"));
-		System.out.println("" + (everbie.getStrength() == 0?"Everbie has no Strength": "Everbie has Strength"));
-		System.out.println("" + (everbie.getToxicity() == 100?"Everbie has Toxicity": "Everbie has no Toxicity"));
+		if(everbie.getImageId() == 0){
+			Log.e("Error", "Wrong starting imageId");
+		}
+		if(everbie.getCharm() != 1){
+			Log.e("Error", "Wrong starting Charm");
+		}
+		if(everbie.getCuteness() != 1){
+			Log.e("Error", "Wrong starting Fullness");
+		}
+		if(everbie.getFullness() != 50){
+			Log.e("Error", "Wrong starting Fullness");
+		}
+		if(everbie.getHappiness() != 50){
+			Log.e("Error", "Wrong starting Happiness");
+		}
+		if(everbie.getHealth() != 20){
+			Log.e("Error", "Wrong starting Health");
+		}
+		if(everbie.getIntelligence() != 1){
+			Log.e("Error", "Wrong starting Intelligence");
+		}
+		if(everbie.getLevel() != 1){
+			Log.e("Error", "Wrong starting Level");
+		}
+		if(everbie.getMaxHealth() != 20){
+			Log.e("Error", "Wrong starting Maxhealth");
+		}
+		if(everbie.getMoney() != 0){
+			Log.e("Error", "Wrong starting Money");
+		}
+		if(everbie.getStamina() != 1){
+			Log.e("Error", "Wrong starting Stamina");
+		}
+		if(everbie.getStrength() != 1){
+			Log.e("Error", "Wrong starting Strength");
+		}
+		if(everbie.getToxicity() > 0){
+			Log.e("Error", "Wrong starting Toxicity");
+		}
+
+		Log.i("Complete", "Test starting variables complete");
+
+	}
+	
+	/**
+	 * Method for testing the change methods in Everbie
+	 */
+	private void testChangeVariables(){
 		
-		everbie.setName("Oscar");
+		Log.i("Start", "Test change variables started");
 		
-		System.out.println("" + (everbie.getName() == null?"Everbie has no name": "Everbie has name"));
-		System.out.println("" + (everbie.getName().equalsIgnoreCase("Oscar")?"Everbie's name isn't Oscar": "Everbie's name is Oscar"));
+		everbie.setName("Kalle");
+		everbie.changeCharm(5);
+		everbie.changeCuteness(5);
+		everbie.changeFullness(5);
+		everbie.changeHappiness(5);
+		everbie.changeHealth(5);
+		everbie.changeIntelligence(5);
+		everbie.changeMaxhealth(5);
+		everbie.changeMoney(5);
+		everbie.changeStamina(5);
+		everbie.changeStrength(5);
+		everbie.changeToxicity(5);
+	
+		if(!everbie.getName().equals("Kalle")){
+			Log.e("Error", "Wrong name after change");
+		}
+		if(everbie.getCharm() != 5){
+			Log.e("Error", "Wrong Charm after change");
+		}
+		if(everbie.getCuteness() != 5){
+			Log.e("Error", "Wrong Fullness after change");
+		}
+		if(everbie.getFullness() != 5){
+			Log.e("Error", "Wrong Fullness after change");
+		}
+		if(everbie.getHappiness() != 5){
+			Log.e("Error", "Wrong Happiness after change");
+		}
+		if(everbie.getHealth() != 5){
+			Log.e("Error", "Wrong Health after change");
+		}
+		if(everbie.getIntelligence() != 5){
+			Log.e("Error", "Wrong Intelligence after change");
+		}
+		if(everbie.getLevel() != 5){
+			Log.e("Error", "Wrong Level after change");
+		}
+		if(everbie.getMaxHealth() != 5){
+			Log.e("Error", "Wrong Maxhealth after change");
+		}
+		if(everbie.getMoney() != 5){
+			Log.e("Error", "Wrong Money after change");
+		}
+		if(everbie.getStamina() != 5){
+			Log.e("Error", "Wrong Stamina after change");
+		}
+		if(everbie.getStrength() != 5){
+			Log.e("Error", "Wrong Strength after change");
+		}
+		if(everbie.getToxicity() != 5){
+			Log.e("Error", "Wrong Toxicity after change");
+		}
+
+		Log.i("Complete", "Test change variables complete");
 		
 	}
+	
+	/**
+	 * Method for testing the OccupationSecends method in Everbie
+	 */
+	private void testOccupiedSeconds(){
+		
+		Log.i("Start", "Test occupation seconds started");
+		
+		everbie.resetOccupied();
+		if(everbie.isOccupied()){
+			Log.e("Error", "Everbie is occupied without having set occupation time");
+		}
+		
+		everbie.setOccupiedSeconds(0);
+		if(everbie.isOccupied()){
+			Log.e("Error", "Everbie is occupied without having set a correct occupation time");
+		}
+		
+		everbie.setOccupiedSeconds(10);
+		if(!everbie.isOccupied()){
+			Log.e("Error", "Occupation time has been set but the Everbie is still not occupied");
+		}
+		
+		Log.i("Complete", "Test occupation seconds complete");
+	}
+	
+	/**
+	 * Method for testing the OccupationSecends method in Everbie
+	 */
+	private void testOccupiedMinutes(){
+		
+		Log.i("Start", "Test occupation Minutes started");
+		
+		everbie.resetOccupied();
+		if(everbie.isOccupied()){
+			Log.e("Error", "Everbie is occupied without having set occupation time");
+		}
+		
+		everbie.setOccupiedMinutes(0);
+		if(everbie.isOccupied()){
+			Log.e("Error", "Everbie is occupied without having set a correct occupation time");
+		}
+		
+		everbie.setOccupiedMinutes(10);
+		if(!everbie.isOccupied()){
+			Log.e("Error", "Occupation time has been set but the Everbie is still not occupied");
+		}
+		
+		Log.i("Complete", "Test occupation seconds complete");
+	}
+	
+	/**
+	 * Method for testing the OccupationSecends method in Everbie
+	 */
+	private void testOccupiedHours(){
+		
+		Log.i("Start", "Test occupation hours started");
+		
+		everbie.resetOccupied();
+		if(everbie.isOccupied()){
+			Log.e("Error", "Everbie is occupied without having set occupation time");
+		}
+		
+		everbie.setOccupiedHours(0);
+		if(everbie.isOccupied()){
+			Log.e("Error", "Everbie is occupied without having set a correct occupation time");
+		}
+		
+		everbie.setOccupiedHours(10);
+		if(!everbie.isOccupied()){
+			Log.e("Error", "Occupation time has been set but the Everbie is still not occupied");
+		}
+		
+		Log.i("Complete", "Test occupation hours complete");
+	}
+	
+	/**
+	 * Method for testing the reset method in Everbie 
+	 */
+	private void testResetEverbie(){
+		
+		Log.i("Start", "Test reset Everbie started");
+		
+		everbie.reset();
+		
+		if(everbie != null){
+			Log.e("Error", "everbie has not been reset");
+		}
+		
+		Log.i("Complete", "Test reset Everbie complete");
+	}
+	/**
+	 * Method for testing the restoreEverbie method in Everbie
+	 */
+	private void testRestoreEverbie(){
+		
+		Log.i("Start", "Test restore Everbie started");
+		
+		int[] values = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+		
+		everbie.restoreEverbie("Nalle", values, true, 1);
+		
+		if(everbie == null){
+			Log.e("Error", "Restoring everbie failed");
+		}
+		
+		Log.i("Complete", "Test restore Everbie complete");
+	}
+	
 }
